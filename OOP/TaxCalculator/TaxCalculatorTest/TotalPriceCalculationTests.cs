1078af5e9686e472b9023dbf317ae581a0d9e690
Miłosz Wojarski <milwojarski@gmail.com>
2021-03-11 10:53:26 +0100

    Add fixed tests

--- OOP/TaxCalculator/TaxCalculatorTest/TotalPriceCalculationTests.cs
+++ OOP/TaxCalculator/TaxCalculatorTest/TotalPriceCalculationTests.cs
@@ -10,10 +10,10 @@ namespace TaxCalculatorTest
         }
 
         [Test]
-        [TestCase(33.48, "ibuprom", 3, 10.12)]
+        [TestCase(31.88, "ibuprom", 3, 10.12)]
         [TestCase(4.97, "watermelon", 1, 4.21, true)]
         [TestCase(5.56, "watermelon", 1, 5.15)]
-        [TestCase(1550.08, "chair", 2, 692.12)]
+        [TestCase(1550.35, "chair", 2, 692.12)]
         [TestCase(1228.61, "chair", 2, 503.53, true)]
         [TestCase(35238.35, "laptop", 5, 5299.0, true)]
         public void TestTotalPriceCalculation(double expectedTotalPrice, string product, int quantity, double productPrice, bool isImported = false)
