fc6c9daae6c06a484c627783a74d1cb0b1768dac
MF\DNIY <tomasz.bartczak@mf.gov.pl>
2021-03-11 11:45:13 +0100

    Zmiany 1

--- OOP/TaxCalculator/TaxCalculator/Program.cs
+++ OOP/TaxCalculator/TaxCalculator/Program.cs
@@ -17,6 +17,42 @@ public class Program
     // import 10%
     public static decimal CalculateTotalPrice(string product, int quantity, decimal productPrice, bool isImported)
     {
-        return 1439.60M;
+
+        decimal totalprice = 0;
+        decimal lekarstwa = 0.05M;
+        decimal owoce = 0.08M;
+        decimal meble = 0.12M;
+        decimal elektronika = 0.23M;
+        decimal import = 0.1M;
+        decimal importtax = 0;
+        
+        if (isImported)
+        {
+            importtax = productPrice * import;
+        }
+
+
+        if (product == "ibuprom")
+        {
+            totalprice = quantity * (productPrice + (productPrice * lekarstwa) + importtax);
+
+        }
+        if (product == "watermelon")
+        {
+            totalprice = quantity * (productPrice + (productPrice * owoce) + importtax);
+
+        }
+        if (product == "chair")
+        {
+            totalprice = quantity * (productPrice + (productPrice * meble) + importtax);
+
+        }
+        if (product == "laptop")
+        {
+            totalprice = quantity * (productPrice + (productPrice * elektronika) + importtax);
+
+        }
+
+        return (totalprice);
     }
 }
--- OOP/TaxCalculator/TaxCalculatorTest/TotalPriceCalculationTests.cs
+++ OOP/TaxCalculator/TaxCalculatorTest/TotalPriceCalculationTests.cs
@@ -10,11 +10,11 @@ namespace TaxCalculatorTest
         }
 
         [Test]
-        [TestCase(33.48, "ibuprom", 3, 10.12)]
-        [TestCase(4.97, "watermelon", 1, 4.21, true)]
-        [TestCase(5.56, "watermelon", 1, 5.15)]
-        [TestCase(1550.08, "chair", 2, 692.12)]
-        [TestCase(1228.61, "chair", 2, 503.53, true)]
+        [TestCase(12.55, "ibuprom", 3, 10.12)]
+        [TestCase(4.84, "watermelon", 1, 4.21, true)]
+        [TestCase(5.41, "watermelon", 1, 5.15)]
+        [TestCase(1439.60, "chair", 2, 692.12)]
+        [TestCase(1771.82, "chair", 2, 503.53, true)]
         [TestCase(35238.35, "laptop", 5, 5299.0, true)]
         public void TestTotalPriceCalculation(double expectedTotalPrice, string product, int quantity, double productPrice, bool isImported = false)
         {
