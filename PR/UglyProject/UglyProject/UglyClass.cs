1b0913646415048bea438945e8c4b64bd3fb7287
Miłosz Wojarski <milwojarski@gmail.com>
2021-03-11 08:54:08 +0100

    Ugly code example

---
+++ PR/UglyProject/UglyProject/UglyClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UglyProject
+{
+    public class UglyClass
+    {
+        public void UglyMethooood()
+        {
+            int a = 2;
+
+            int @int = a * 2 / 2; //zbędny komentarz
+
+            Console.WriteLine(@int);
+        }
+    }
+}
